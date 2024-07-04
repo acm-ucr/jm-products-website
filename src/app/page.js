908adb4538ca78563fdd5ae777a9f38e40d56a51
SafeDuck <schopra579@gmail.com>
2024-07-03 19:44:51 -0700

    deleted bluebox

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,10 @@ import Landing from "@/components/Landing";
 import AboutJM from "@/components/home/AboutJM";
 import OurManufacturingServices from "@/components/home/OurManufacturingServices";
 import PhotoStrip from "@/components/home/PhotoStrip";
-import BlueBoxes from "@/components/BlueBoxes";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
-      {/* Props go here */}
-      <BlueBoxes />
       <Landing />
       <AboutJM />
       <OurManufacturingServices />
