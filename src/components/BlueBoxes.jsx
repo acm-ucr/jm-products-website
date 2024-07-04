e0ca4d6a735ce7731784b849632516d9878f9158
SafeDuck <schopra579@gmail.com>
2024-07-03 18:00:34 -0700

    description of your changes

--- src/components/BlueBoxes.jsx
+++ src/components/BlueBoxes.jsx
@@ -5,7 +5,7 @@ const BlueBoxes = () => {
   return (
     <div className="bg-red-200 w-2/3 h-32">
       <div className="bg-jm-blue-100 ">
-        <p>Prop1</p>
+        <p>Prop12</p>
       </div>
       <div className="bg-jm-blue-200">
         <p>Prop2</p>
