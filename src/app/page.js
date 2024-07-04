00a3ed18e98bec49dbe218ee6684e3337e6b3b39
Jooahn Park <jpark8577@gmail.com>
2024-07-03 18:39:27 -0700

    Add Component: BlueBoxes

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
       {/* Props go here */}
-      <BlueBoxes />
+      <BlueBoxes text1="Box1" text2="Box2" text3="Box3" />
       <Landing />
       <AboutJM />
       <OurManufacturingServices />
--- src/components/BlueBoxes.jsx
+++ src/components/BlueBoxes.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 // Props go here
-const BlueBoxes = () => {
+const BlueBoxes = ({ text1, text2, text3 }) => {
   return (
-    <div className="bg-red-200 w-2/3 h-32">
-      <div className="bg-jm-blue-100 ">
-        <p>Prop1</p>
+    <div className="bg-red-200 w-2/3 h-32 flex justify-center items-center">
+      <div className="bg-jm-blue-100 w-1/3 h-32 flex justify-center items-center">
+        <p className="text-white text-lg font-bold">{text1}</p>
       </div>
-      <div className="bg-jm-blue-200">
-        <p>Prop2</p>
+      <div className="bg-jm-blue-200 w-72 h-32 flex justify-center items-center rounded-full mx-auto">
+        <p className="text-white text-lg font-bold">{text2}</p>
       </div>
-      <div className="bg-jm-blue-300">
-        <p>Prop3</p>
+      <div className="bg-jm-blue-300 w-1/3 h-32 flex justify-center items-center">
+        <p className="text-white text-lg font-bold">{text3}</p>
       </div>
     </div>
   );
