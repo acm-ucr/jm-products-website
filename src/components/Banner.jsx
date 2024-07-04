08aae690bef37e8a55a70fa403c957d84bc14796
unknown <aviambequette@gmail.com>
2024-07-03 19:53:58 -0700

    fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,10 +14,10 @@ const Banner = ({ text }) => {
         </h1>
       </div>
       <div className="absolute start-0 end-0 inset-y-2/3 pr-4">
-        <Image src={BannerDecor1} alt="Banner Decor" className="w-1/6" />
+        <Image src={BannerDecor1} alt="Banner Decor 1" className="w-1/6" />
       </div>
       <div className="absolute bottom-0 right-0 inset-y-3/4">
-        <Image src={BannerDecor2} alt="Banner Decor" className="w-full" />
+        <Image src={BannerDecor2} alt="Banner Decor 2" className="w-full" />
       </div>
     </div>
   );
