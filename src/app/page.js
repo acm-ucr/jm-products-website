375f96b9c0173e518af7323db362a3a88dbec409
Ezkron <121210286+Ezkron@users.noreply.github.com>
2024-07-03 21:32:22 -0700

    message

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import BlueBoxes from "@/components/BlueBoxes";
+
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      <div className="flex justify-center items-center space-x-4 my-10 h-20"></div>
+      <BlueBoxes text1={"Box 1"} text2={"Box 2"} text3={"Box 3"} />
+    </>
+  );
 };
 
 export default Home;
