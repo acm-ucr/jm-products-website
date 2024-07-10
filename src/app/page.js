a1fe85e7ee1acc909dfb81dd67708f9766a2c357
Jooahn Park <jpark8577@gmail.com>
2024-07-10 03:25:48 -0700

    Added Component: Button

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import Landing from "@/components/Landing";
 import AboutJM from "@/components/home/AboutJM";
 import OurManufacturingServices from "@/components/home/OurManufacturingServices";
 import PhotoStrip from "@/components/home/PhotoStrip";
+import Button from "@/components/Button";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
+      <Button text={"Learn More"} link={"/about"} />
       <Landing />
       <AboutJM />
       <OurManufacturingServices />
