92fd4af2b5ba26b965ab97accbf7a5d46292cfe0
Sujay Subramanyam <ssubr022@gmail.com>
2024-07-11 20:54:03 -0700

    Finished my component

--- src/components/home/PhotoStrip.jsx
+++ src/components/home/PhotoStrip.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const PhotoStrip = () => {
-  return <div>PhotoStrip</div>;
+  // URL of the image
+  const imageSrc = "/images/JMPhotoStrip.webp";
+
+  return (
+    <div className="flex">
+      <img
+        src={imageSrc}
+        alt="Random"
+        className="w-full max-w-full rounded-lg shadow-lg object-cover"
+        style={{ height: "auto" }}
+      />
+    </div>
+  );
 };
 
 export default PhotoStrip;
