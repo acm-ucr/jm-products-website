5b026b82a734c9784e9e614e501716227c768ff0
CruzL092 <cruzbusiness760@gmail.com>
2024-07-11 20:52:10 -0700

    Ran npm run format for the workflow

--- src/components/harnesses/HarnessAssembly.jsx
+++ src/components/harnesses/HarnessAssembly.jsx
@@ -4,7 +4,7 @@ const HarnessAssembly = () => {
   return (
     <div>
       <h1 className="font-sans flex justify-center text-transparent bg-clip-text bg-gradient-to-r from-jm-blue-300 via-jm-blue-200 to-jm-blue-100 text-3xl mb-6 tracking-wide underline-offset-8 whitespace-break-spaces underline decoration-jm-blue-200 ">
-        <b>J&M Products Harness Assembly Overview             </b>
+        <b>J&M Products Harness Assembly Overview </b>
       </h1>
       <div className="flex justify-center">
         <ul className=" text-left text-sm list-disc flex flex-col font-sans w-5/12 tracking-wider">
