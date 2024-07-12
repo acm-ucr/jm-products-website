04eed5b0a9fcd75db6f4289477cff07adf10a016
Ezkron <eddie.leung246@gmail.com>
2024-07-11 22:54:03 -0700

    Header has text

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import BlueBoxes from "@/components/BlueBoxes";
+import Header from "@/components/about/Header";
 
 const Home = () => {
   return (
     <>
       <div className="flex justify-center items-center space-x-4 my-10 h-20"></div>
+      <Header />
       <BlueBoxes text1={"Box 1"} text2={"Box 2"} text3={"Box 3"} />
     </>
   );
---
+++ src/components/about/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mt-8 ml-8">
+      <h1 className="text-4x1 font-bold tracking-widest text-blue-700">
+        WHO WE ARE
+      </h1>
+      <hr className="border-t-2 border-blue-700 mt-2 w-3/4" />
+    </div>
+  );
+};
+export default Header;
