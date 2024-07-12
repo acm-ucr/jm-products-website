893f4b53b97310820cdaece0c94e464e394af121
Anish Nalluri <anall003@ucr.edu>
2024-07-11 21:06:25 -0700

    Implement Customer Concerns

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
-import Landing from "@/components/Landing";
-import AboutJM from "@/components/home/AboutJM";
-import OurManufacturingServices from "@/components/home/OurManufacturingServices";
-import PhotoStrip from "@/components/home/PhotoStrip";
+import React from "react";
+import CustomerConcerns from "@/components/outsourcing/CustomerConcerns";
 
-const Home = () => {
+const Page = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-10">
-      <Landing />
-      <AboutJM />
-      <OurManufacturingServices />
-      <PhotoStrip />
+    <div>
+      <h1>Page</h1>
+      <CustomerConcerns />
     </div>
   );
 };
-export default Home;
+
+export default Page;
--- src/components/outsourcing/CustomerConcerns.jsx
+++ src/components/outsourcing/CustomerConcerns.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const CustomerConcerns = () => {
-  return <div>CustomerConcerns</div>;
+  return (
+    <div>
+      <h2>Normal Customer Concerns:</h2>
+      <ul>
+        <li>
+          Our documentation doesn&apos;t always reflect the current state of the
+          manufactured product.
+        </li>
+        <li>
+          Engineering resources are often too thin to update specifications
+          accordingly.
+        </li>
+        <li>
+          Since we&apos;ve been building in house what do we do with our
+          residual inventory?
+        </li>
+        <li>How do we convey to J&amp;M what our requirements are?</li>
+      </ul>
+    </div>
+  );
 };
 
 export default CustomerConcerns;
