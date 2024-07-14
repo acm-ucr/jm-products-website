770183a578c7adfec0c88bb113e9a26bba3e137d
Ezkron <eddie.leung246@gmail.com>
2024-07-13 21:47:19 -0700

    header file now is up to date with font, size, and color

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,3 @@
-import WhoWeAre from "@/components/about/WhoWeAre";
-import React from "react";
 import WhyJM from "@/components/about/WhyJM";
 import Information from "@/components/about/Information";
 import Header from "@/components/about/Header";
@@ -7,7 +5,6 @@ import Header from "@/components/about/Header";
 const page = () => {
   return (
     <div className="w-full flex flex-col justify-center  gap-10">
-      <WhoWeAre />
       <WhyJM />
       <Information />
       <Header />
--- src/components/about/Header.jsx
+++ src/components/about/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <div className="mt-8 ml-8">
-      <h1 className="text-4x1 font-bold tracking-widest text-blue-700">
+      <p className="font-sans-hk text-[48px] font-bold tracking-widest text-jm-blue-200">
         WHO WE ARE
-      </h1>
-      <hr className="border-t-2 border-blue-700 mt-2 w-3/4" />
+        {title}
+      </p>
+      <hr className="border-t-2 border-jm-blue-200 mt-2 w-3/4" />
     </div>
   );
 };
