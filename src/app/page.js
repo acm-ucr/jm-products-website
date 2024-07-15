fdeb74e8101d830b1a5b8e022697929dcd88aebf
Jooahn Park <jpark8577@gmail.com>
2024-07-15 02:42:56 -0700

    Added Service Description Component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Landing from "@/components/Landing";
 import AboutJM from "@/components/home/AboutJM";
 import OurManufacturingServices from "@/components/home/OurManufacturingServices";
 import PhotoStrip from "@/components/home/PhotoStrip";
+import ServiceDescription from "@/components/home/ServiceDescription";
+import ClampsAndBracket from "../../public/images/home/ClampsAndBracket.webp";
 
 const Home = () => {
   return (
@@ -9,6 +11,20 @@ const Home = () => {
       <Landing />
       <AboutJM />
       <OurManufacturingServices />
+      <ServiceDescription
+        img={ClampsAndBracket}
+        title="Clamps and Bracket"
+        desc="J&M Line Support Clamps and Bracket solutions are utilized to support hydraulic, electrical and pneumatic systems in extreme environmental conditions (temperature variation, fluid exposure, vibration, etc)."
+        link="/about"
+        float="left"
+      />
+      <ServiceDescription
+        img={ClampsAndBracket}
+        title="Clamps and Bracket"
+        desc="J&M Line Support Clamps and Bracket solutions are utilized to support hydraulic, electrical and pneumatic systems in extreme environmental conditions (temperature variation, fluid exposure, vibration, etc)."
+        link="/about"
+        float="right"
+      />
       <PhotoStrip />
     </div>
   );
--- src/components/home/ServiceDescription.jsx
+++ src/components/home/ServiceDescription.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Image from "next/image";
+import Button from "../Button";
 
-const ServiceDescription = () => {
-  return <div>ServiceDescription</div>;
+const ServiceDescription = ({ img, title, desc, link, float }) => {
+  return (
+    <div className="w-full bg-jm-blue-200">
+      <div
+        className={`flex flex-row px-36 ${
+          float === "right" ? "flex-row-reverse" : ""
+        }`}
+      >
+        <Image
+          src={img}
+          alt="Service Description Picture"
+          className="w-1/4 px-5"
+        />
+        <div className="flex flex-col">
+          <h2
+            className={`text-white text-left text-3xl font-bold ${
+              float === "right" ? "text-right" : ""
+            }`}
+            style={{ letterSpacing: "0.13em" }}
+          >
+            {title}
+          </h2>
+          <p
+            className={`text-white text-left py-9 ${
+              float === "right" ? "text-right text-balance" : ""
+            }`}
+          >
+            {desc}
+          </p>
+          <div
+            className={`flex ${float === "right" ? "flex justify-end" : ""}`}
+          >
+            <Button text="Learn More" link={link} className="bg-jm-blue-200" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDescription;
