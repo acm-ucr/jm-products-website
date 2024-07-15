add4edce79deddc0ac1c0ebffba8d94aab6de80b
SafeDuck <schopra579@gmail.com>
2024-07-14 19:22:43 -0700

    Footer Fix

--- src/data/Footer.js
+++ src/data/Footer.js
@@ -3,30 +3,18 @@ import { AiFillFacebook } from "react-icons/ai";
 
 export const data = [
   {
-    icon: "sales@jmproducts.com",
-    link: "sales@jmproducts.com",
     contact: "sales@jmproducts.com",
-  },
-  {
     icon: <FaInstagram />,
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    link: "https://www.instagram.com/jmproducts_california/",
   },
   {
-    icon: "+1-800-624-2198",
-    link: "+1-800-624-2198",
     contact: "+1-800-624-2198",
-  },
-  {
     icon: <FaLinkedin />,
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    link: "https://www.linkedin.com/company/j-&-m-products-inc-/",
   },
   {
-    icon: "+1-818-837-0205",
-    link: "+1-818-837-0205",
     contact: "+1-818-837-0205",
-  },
-  {
     icon: <AiFillFacebook />,
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    link: "https://www.facebook.com/JMProductsInc",
   },
 ];
