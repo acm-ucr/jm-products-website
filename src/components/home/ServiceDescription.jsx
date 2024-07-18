41482e8b1caa124f82e47f655cad87cc3d9ff745
Jooahn Park <jpark8577@gmail.com>
2024-07-18 12:41:19 -0700

    Removed background and padding

--- src/components/home/ServiceDescription.jsx
+++ src/components/home/ServiceDescription.jsx
@@ -4,46 +4,40 @@ import Button from "../Button";
 
 const ServiceDescription = ({ img, title, desc, link, float }) => {
   return (
-    <div className="w-full bg-jm-blue-200">
-      <div
-        className={`flex flex-row px-36 ${
-          float === "right" ? "flex-row-reverse" : ""
-        }`}
-      >
-        <Image
-          src={img}
-          alt="Service Description Picture"
-          className={`w-1/4 pr-5 ${float === "right" ? "pl-5" : ""}`}
-        />
-        <div className="flex flex-col">
-          <h2
-            className={`text-white text-left text-3xl font-bold ${
-              float === "right" ? "text-right" : ""
-            }`}
-            style={{ letterSpacing: "0.13em" }}
-          >
-            {title}
-          </h2>
-          <p
-            className={`text-white text-left py-9 text-balance ${
-              float === "right" ? "text-right" : ""
-            }`}
-          >
-            {desc}
-          </p>
-          <div
-            className={`flex ${float === "right" ? "flex justify-end" : ""}`}
-          >
-            <Button
-              text="Learn More"
-              link={link}
-              bgColor={"bg-jm-blue-200"}
-              borderColor={"border-white"}
-              textColor={"text-white"}
-              hoverBg={"group hover:bg-white"}
-              hoverText={"group-hover:text-jm-blue-200"}
-            />
-          </div>
+    <div
+      className={`flex flex-row ${float === "right" ? "flex-row-reverse" : ""}`}
+    >
+      <Image
+        src={img}
+        alt="Service Description Picture"
+        className={`w-1/4 pr-5 ${float === "right" ? "pl-5" : ""}`}
+      />
+      <div className="flex flex-col">
+        <h2
+          className={`text-white text-left text-3xl font-bold ${
+            float === "right" ? "text-right" : ""
+          }`}
+          style={{ letterSpacing: "0.13em" }}
+        >
+          {title}
+        </h2>
+        <p
+          className={`text-white text-left py-9 text-balance ${
+            float === "right" ? "text-right" : ""
+          }`}
+        >
+          {desc}
+        </p>
+        <div className={`flex ${float === "right" ? "flex justify-end" : ""}`}>
+          <Button
+            text="Learn More"
+            link={link}
+            bgColor={"bg-jm-blue-200"}
+            borderColor={"border-white"}
+            textColor={"text-white"}
+            hoverBg={"group hover:bg-white"}
+            hoverText={"group-hover:text-jm-blue-200"}
+          />
         </div>
       </div>
     </div>
