0cece10fd18f97088f931e2ea90365edd02d901e
Anish Nalluri <anall003@ucr.edu>
2024-07-18 21:34:57 -0700

    Changed font and styling

--- src/components/outsourcing/CustomerConcerns.jsx
+++ src/components/outsourcing/CustomerConcerns.jsx
@@ -2,22 +2,27 @@ import React from "react";
 
 const CustomerConcerns = () => {
   return (
-    <div>
-      <h2>Normal Customer Concerns:</h2>
-      <ul>
-        <li>
+    <div className="bg-transparent">
+      <h2 className="text-xl font-semibold mb-10 pl-1">
+        Normal Customer Concerns:
+      </h2>
+      <ul className="list-disc pl-5 space-y-1 ml-20">
+        <li className="text-sm">
           Our documentation doesn&apos;t always reflect the current state of the
-          manufactured product.
+          <br /> manufactured product.
         </li>
-        <li>
+        <li className="text-sm">
           Engineering resources are often too thin to update specifications
-          accordingly.
+          <br /> accordingly.
         </li>
-        <li>
+        <li className="text-sm">
           Since we&apos;ve been building in house what do we do with our
-          residual inventory?
+          residual
+          <br /> inventory?
         </li>
-        <li>How do we convey to J&amp;M what our requirements are?</li>
+        <li className="text-sm">
+          How do we convey to J&amp;M what our requirements are?
+        </li>
       </ul>
     </div>
   );
