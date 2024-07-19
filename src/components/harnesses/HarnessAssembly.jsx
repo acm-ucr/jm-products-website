2637f18944090dd0ccf5d0615d339a2a1a069b1c
CruzL092 <cruzbusiness760@gmail.com>
2024-07-18 18:26:23 -0700

    Refactored HarnessAssembly Component

--- src/components/harnesses/HarnessAssembly.jsx
+++ src/components/harnesses/HarnessAssembly.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { data } from "../../data/HarnessAssembly.js";
 
 const HarnessAssembly = () => {
   return (
@@ -8,56 +9,9 @@ const HarnessAssembly = () => {
       </h1>
       <div className="flex justify-center">
         <ul className=" text-left text-sm list-disc flex flex-col font-hk w-5/12 tracking-wider">
-          <li>
-            {" "}
-            Assemblies ranging from simple and single end terminated harnesses
-            to multi-leg harnesses.
-          </li>
-          <li>Operators trained to all aspects of IPC-WHMA-A620.</li>
-          <li>
-            {" "}
-            Harnesses are 100% electrically tested using the latest in test
-            technology.
-          </li>
-          <li>
-            {" "}
-            Testing includes HI-POT. Insulation resistance, connection
-            resistance, continuity, component verification and measurement.
-          </li>
-          <li>
-            {" "}
-            Familiar with virtually all types of Aerospace & Military Connectors
-            such as D38999 / MIL-DTL-38999 / MIL-C-38999, M24308 / MIL-DTL-24308
-            / MIL-C-24308 / MIL-PRF-24308, MIL-DTL-26482 /MIL-C-26482, and
-            MIL-DTL-5015 / MIL-C-5015.
-          </li>
-          <li>
-            {" "}
-            Familiar with many types of commercial/industrial connectors from
-            companies such as Deutsch, Molex, TE connectivity, Packard, JST,
-            etc.
-          </li>
-          <li>Automated Cut & Strip Wire Processing Machine</li>
-          <li>Wire hot stamping</li>
-          <li>Pneumatic wire stripping</li>
-          <li> Pneumatic contact crimping</li>
-          <li> Crimp pull testing & crimp height verification </li>
-          <li>
-            {" "}
-            Wide selection of labeling capabilities including Thermal, Dot
-            Matrix, TYCO TMS (M23053), WH Brady Marking, etc
-          </li>
-          <li> Electro-Mechanical Assembly</li>
-          <li> Overbraiding Capability</li>
-          <li>
-            {" "}
-            Overmolding/Potting - Ability to overmold breakouts molded strain
-            reliefs, grommets & components and to encapsulate via a variety of
-            potting compounds{" "}
-          </li>
-          <li> Box Build Capability</li>
-          <li> Prototyping</li>
-          <li> Short Run Production</li>
+          {data.map((item, index) => (
+            <li key={index}> {item}</li>
+          ))}
         </ul>
       </div>
     </div>
--- src/data/HarnessAssembly.js
+++ src/data/HarnessAssembly.js
@@ -2,6 +2,20 @@ export const data = [
   "Assemblies ranging from simple and single end terminated harnesses to multi-leg harnesses.",
   "Operators trained to all aspects of IPC-WHMA-A620.",
   "Harnesses are 100% electrically tested using the latest in test technology. Testing includes HI-POT. Insulation resistance, connection resistance, continuity, component verification, and measurement.",
+  "Familiar with virtually all types of Aerospace & Military Connectors such as D38999 / MIL-DTL-38999 / MIL-C-38999, M24308 / MIL-DTL-24308 / MIL-C-24308 / MIL-PRF-24308, MIL-DTL-26482 /MIL-C-26482, and MIL-DTL-5015 / MIL-C-5015.",
+  "Familiar with many types of commercial/industrial connectors from companies such as Deutsch, Molex, TE connectivity, Packard, JST, etc.",
+  "Automated Cut & Strip Wire Processing Machine",
+  "Wire hot stamping",
+  "Pneumatic wire stripping",
+  "Pneumatic contact crimping",
+  "Crimp pull testing & crimp height verification",
+  "Wide selection of labeling capabilities including Thermal, Dot Matrix, TYCO TMS (M23053), WH Brady Marking, etc",
+  "Electro-Mechanical Assembly",
+  "Overbraiding Capability",
+  "Overmolding/Potting - Ability to overmold breakouts molded strain reliefs, grommets & components and to encapsulate via a variety of potting compounds",
+  "Box Build Capability",
+  "Prototyping",
+  "Short Run Production",
 ];
 
 // Add the rest of the bullet points here. Delete this comment once you push
