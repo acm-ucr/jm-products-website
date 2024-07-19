f8fa1fac2143cc2fee167794e3d6cc1bbdd42ca1
Ezkron <eddie.leung246@gmail.com>
2024-07-18 21:33:41 -0700

    Banner Text added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,14 @@
+import WhoWeAre from "@/components/about/WhoWeAre";
+import React from "react";
 import WhyJM from "@/components/about/WhyJM";
 import Information from "@/components/about/Information";
-import Header from "@/components/about/Header";
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col justify-center gap-10">
+    <div className="w-full flex flex-col justify-center items-center gap-10">
+      <WhoWeAre />
       <WhyJM />
       <Information />
-      <Header />
     </div>
   );
 };
