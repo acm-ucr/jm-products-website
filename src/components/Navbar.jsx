e0c00213660ec2a2baadcd576003916e7b07a966
unknown <aviambequette@gmail.com>
2024-07-18 20:12:17 -0700

    initial logo setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "@/public/svg/J&MLogo.svg";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navigation = () => {
+  return (
+    <Navbar className="flex">
+      <NavbarBrand className="p-3">
+        <Image src={Logo} alt="Logo" className="w-full" />
+      </NavbarBrand>
+    </Navbar>
+  );
 };
 
-export default Navbar;
+export default Navigation;
