af8eb64510887aa5313c3c15d50e02efc495c84d
Ezkron <eddie.leung246@gmail.com>
2024-07-18 21:26:51 -0700

    header changes

--- src/components/about/Header.jsx
+++ src/components/about/Header.jsx
@@ -4,7 +4,6 @@ const Header = ({ title }) => {
   return (
     <div className="mt-7 ml-7">
       <p className="font-hk bg-gradient-to-r from-jm-blue-300 to-jm-blue-100 bg-clip-text text-transparent text-5xl tracking-widest font-bold">
-        WHO WE ARE
         {title}
       </p>
       <hr className="border-t-4 border-jm-blue-200 mt-0 w-3/4" />
