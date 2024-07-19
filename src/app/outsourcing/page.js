c83fb9e6822264fd10108466dc807fbc2eaa3da6
SafeDuck <schopra579@gmail.com>
2024-07-19 16:15:59 -0700

    Tweak CustomerConcerns.jsx

--- src/app/outsourcing/page.js
+++ src/app/outsourcing/page.js
@@ -2,13 +2,18 @@ import React from "react";
 import Banner from "@/components/Banner";
 import CustomerConcerns from "@/components/outsourcing/CustomerConcerns";
 import Description from "@/components/Description";
+import BlueText from "@/components/outsourcing/BlueText";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
       <Banner />
+      <BlueText />
       <CustomerConcerns />
       <Description />
+      <BlueText />
+      <Description />
+      <BlueText />
     </div>
   );
 };
--- src/components/outsourcing/CustomerConcerns.jsx
+++ src/components/outsourcing/CustomerConcerns.jsx
@@ -2,23 +2,22 @@ import React from "react";
 
 const CustomerConcerns = () => {
   return (
-    <div className="bg-transparent">
-      <h2 className="text-xl font-semibold mb-10 pl-1">
+    <div className="w-3/4 xl:w-2/3 font-hk flex flex-col justify-center items-center gap-10">
+      <h2 className="text-xl font-semibold w-full">
         Normal Customer Concerns:
       </h2>
-      <ul className="list-disc pl-5 space-y-1 ml-20">
+      <ul className="list-disc space-y-1">
         <li className="text-sm">
           Our documentation doesn&apos;t always reflect the current state of the
-          <br /> manufactured product.
+          manufactured product.
         </li>
         <li className="text-sm">
           Engineering resources are often too thin to update specifications
-          <br /> accordingly.
+          accordingly.
         </li>
         <li className="text-sm">
           Since we&apos;ve been building in house what do we do with our
-          residual
-          <br /> inventory?
+          residual inventory?
         </li>
         <li className="text-sm">
           How do we convey to J&amp;M what our requirements are?
