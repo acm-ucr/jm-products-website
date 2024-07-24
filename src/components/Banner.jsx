6aa2a716ed38f8bc417f19bb01a38e0e78aab93c
Avia Bequette <aviabequette@Avias-MacBook-Pro.local>
2024-07-25 01:21:12 +0900

    responsiveness fix on right banner decor

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = ({ text }) => {
       <div className="absolute start-0 end-0 inset-y-2/3 pr-4">
         <Image src={BannerDecor1} alt="Banner Decor 1" className="w-1/6" />
       </div>
-      <div className="absolute end-0 inset-y-2/3 w-1/5">
+      <div className="absolute w-1/6 -bottom-7 md:-bottom-10 lg:-bottom-20 right-0">
         <Image src={BannerDecor2} alt="Banner Decor 2" className="w-full" />
       </div>
     </div>
