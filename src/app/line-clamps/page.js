5dfe058875138409f2c49bf7f5df1789bf5f7363
Sujay Subramanyam <ssubr022@gmail.com>
2024-07-25 20:58:57 -0700

    text banner complete

--- src/app/line-clamps/page.js
+++ src/app/line-clamps/page.js
@@ -1,14 +1,16 @@
 import React from "react";
-
 import Banner from "@/components/Banner";
 import LineClamps from "@/components/clamps/LineClamps";
 import PartNumberSeries from "@/components/clamps/PartNumberSeries";
+import LogoStrip from "@/components/clamps/LogoStrip";
+
 const page = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
       <Banner />
       <LineClamps />
       <PartNumberSeries />
+      <LogoStrip />
     </div>
   );
 };
