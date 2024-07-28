8351f71582176f9f0fe7655dc8eebba79dfdbadb
Jooahn Park <jpark8577@gmail.com>
2024-07-27 18:37:48 -0700

    Fixed comments and corrected Button functionality

--- src/components/home/OurManufacturingServices.jsx
+++ src/components/home/OurManufacturingServices.jsx
@@ -1,54 +1,65 @@
 import React from "react";
 import Image from "next/image";
+import GearIcon from "@/public/svg/GearIcon.svg";
+import GearIconBg from "@/public/svg/GearIconBackground.svg";
 import ServiceDescription from "@/components/home/ServiceDescription";
-import ClampsAndBracket from "../../../public/images/home/ClampsAndBracket.webp";
-import WireHarnesses from "../../../public/images/home/WireHarnesses.webp";
+import ClampsAndBracket from "@/public/images/home/ClampsAndBracket.webp";
+import WireHarnesses from "@/public/images/home/WireHarnesses.webp";
 
 const OurManufacturingServices = () => {
   return (
-    <div className="w-full bg-jm-blue-200">
-      <div className="flex items-center justify-center">
-        <Image
-          src="/svg/GearIcon.svg"
-          alt="Gear Icon"
-          className="relative left-6 top-1"
-          style={{ zIndex: 1 }}
-          width={32}
-          height={48}
-        />
-        <Image
-          src="/svg/GearIconBackground.svg"
-          alt="Gear Icon Background"
-          className="relative right-4 top-1"
-          width={48}
-          height={56}
-        />
-        <p
-          className="text-white font-bold text-4xl py-32"
-          style={{ letterSpacing: "0.13em" }}
-        >
-          Our Manufacturing Services
-        </p>
-      </div>
-      <div className="px-40 pb-32">
-        <ServiceDescription
-          img={ClampsAndBracket}
-          title="Clamps and Bracket"
-          desc={[
-            "J&M Line Support ",
-            <strong key="bold">Clamps and Bracket</strong>,
-            " solutions are utilized to support hydraulic, electrical and pneumatic systems in extreme environmental conditions (temperature variation, fluid exposure, vibration, etc).",
-          ]}
-          link="/line-clamps"
-          float="left"
-        />
-        <ServiceDescription
-          img={WireHarnesses}
-          title="Wire Harnesses"
-          desc="J&M Products manufactures a variety of wire harnesses serving a broad range of customers including class 8 trucks, aircraft systems, marine, medical, vehicle accessories, and utility body manufacturers."
-          link="/wire-harnesses"
-          float="right"
-        />
+    <div className="w-full bg-jm-blue-200 flex justify-center items-center">
+      <div className="w-5/6">
+        <div className="flex items-center justify-center space-x-4">
+          <div className="relative w-12 h-12">
+            <Image
+              src={GearIconBg}
+              alt="Gear Icon Background"
+              className="absolute"
+            />
+            <Image
+              src={GearIcon}
+              alt="Gear Icon"
+              className="relative left-1/2 transform -translate-x-1/2 top-2.5 z-10"
+            />
+          </div>
+          <p className="text-white font-bold text-4xl py-32 tracking-widest">
+            Our Manufacturing Services
+          </p>
+        </div>
+        <div className="mb-20">
+          <ServiceDescription
+            img={ClampsAndBracket}
+            title="Clamps and Bracket"
+            desc={[
+              "J&M Line Support ",
+              <strong key="bold">Clamps and Bracket</strong>,
+              " solutions are utilized to support hydraulic, electrical and pneumatic systems in extreme environmental conditions (temperature variation, fluid exposure, vibration, etc).",
+            ]}
+            extendedDesc={[
+              <>
+                J&M Line Support <strong key="bold">Clamps and Bracket</strong>{" "}
+                solutions are utilized to support hydraulic, electrical and
+                pneumatic systems in extreme environmental conditions
+                (temperature variation, fluid exposure, vibration, etc).
+              </>,
+              "Reliable, proven, off the shelf or designed to the specific application, J&M line clamps provide fast economical methods of installing wire harnesses, hydraulic lines, and a variety of other applications.",
+              "In addition, J&M can customize any required mounting brackets where vibration or loads would exceed clamp, component design limits, or if compound angles are required.",
+            ]}
+            float="left"
+          />
+          <ServiceDescription
+            img={WireHarnesses}
+            title="Wire Harnesses"
+            desc="J&M Products manufactures a variety of wire harnesses serving a broad range of customers including class 8 trucks, aircraft systems, marine, medical, vehicle accessories, and utility body manufacturers."
+            extendedDesc={[
+              "J&M Products manufactures a variety of wire harnesses serving a broad range of customers including class 8 trucks, aircraft systems, marine, medical, vehicle accessories, and utility body manufacturers.",
+              "Wire harnesses are highly customized to the application per customer specifications; J&M can provide expert assistance during the prototyping and short run phase, ahead of production volumes.",
+              "J&M personnel are extensively trained using WHMA/IPC-A-620 acceptance criteria. The team approach is used to solve complex manufacturing issues. Lean Manufacturing principles are employed throughout the production cycle ensuring a low cost and high quality product.",
+            ]}
+            float="right"
+          />
+        </div>
       </div>
     </div>
   );
