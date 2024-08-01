edeaa680d32306f762720bb4b62da361175163fb
Ezkron <eddie.leung246@gmail.com>
2024-08-01 02:11:54 -0700

    used w-3/4,flex, flex-col, gap-10.

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -3,25 +3,28 @@ import Header from "./Header";
 
 const WhoWeAre = () => {
   return (
-    <div className="p-6">
+    <div className="w-3/4 flex flex-col gap-10 mx-auto">
       <Header title="WHO WE ARE" size="text-4xl" />
-      <section className="text-center my-8">
-        <p className="text-base font-medium mb-4">
+      <section className="text-left">
+        <p className="text-justify font-medium mb-6 leading-relaxed">
           J&M Products is dedicated to the turnkey manufacturing and marketing
-          of high-quality products and services. Through leadership, innovation,
-          and continuous improvement, we are able to achieve the highest level
-          of customer satisfaction. Our ISO 9001:2015/ISO 14001 recognized
-          quality management system applies to all aspects of the company. Our
-          goal is to deliver defect-free product on-time at the most competitive
-          price possible.
+          of high quality products and services. Through leadership, innovation
+          and continuous improvement we are able to achieve the highest level of
+          customer satisfaction. Our AS9100/ISO9001 approved quality management
+          system applies to all aspects of the company. Our goal is to deliver
+          defect-free product on-time at the most competitive price possible.
         </p>
+        <div className="text-left mb-8">
+          <p className="font-hk bg-gradient-to-r from-jm-blue-300 to-jm-blue-100 bg-clip-text text-transparent ${size} tracking-widest font-bold">
+            Brochures
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
         <img
           src="/images/about/JMbrochures.webp"
           alt="Brochures"
           className="w-full max-w-3xl"
         />
-        <p className="text-lg font-bold mb-6">Brochures</p>
-        <div className="flex justify-center"></div>
       </section>
     </div>
   );
