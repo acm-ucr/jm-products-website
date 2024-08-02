a7ba0e50d441c524d3723bbecb0d0de159a9d25a
Sujay Subramanyam <ssubr022@gmail.com>
2024-08-01 21:42:15 -0700

    hopefully it works this time

--- src/components/clamps/LogoStrip.jsx
+++ src/components/clamps/LogoStrip.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 const LogoStrip = () => {
-  return <div>LogoStrip</div>;
+  return (
+    <div className="flex justify-between p-4">
+      <Image
+        src="/images/lineclamps/companies/NASLogo.webp"
+        alt="SAE Logo"
+        width={180}
+        height={92}
+      />
+      <Image
+        src="/images/lineclamps/companies/ANSILogo.webp"
+        alt="ANSI Logo"
+        width={238}
+        height={100}
+      />
+      <Image
+        src="/images/lineclamps/companies/SAELogo.webp"
+        alt="NAS Logo 3"
+        width={106}
+        height={67}
+      />
+    </div>
+  );
 };
 
 export default LogoStrip;
