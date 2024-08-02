ff7a9400368156a8da4452fa62987db05ff97be1
Sujay Subramanyam <ssubr022@gmail.com>
2024-08-01 21:51:36 -0700

    ANOTHER ONE

--- src/components/clamps/LogoStrip.jsx
+++ src/components/clamps/LogoStrip.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import NASLogo from "/images/lineclamps/companies/NASLogo.webp";
+import ANSILogo from "/images/lineclamps/companies/ANSILogo.webp";
+import SAELogo from "/images/lineclamps/companies/SAELogo.webp";
 
 const LogoStrip = () => {
   return (
     <div className="flex justify-between p-4">
-      <Image
-        src="/images/lineclamps/companies/NASLogo.webp"
-        alt="SAE Logo"
-        width={180}
-        height={92}
-      />
-      <Image
-        src="/images/lineclamps/companies/ANSILogo.webp"
-        alt="ANSI Logo"
-        width={238}
-        height={100}
-      />
-      <Image
-        src="/images/lineclamps/companies/SAELogo.webp"
-        alt="NAS Logo 3"
-        width={106}
-        height={67}
-      />
+      <Image src={NASLogo} alt="SAE Logo" width={180} height={92} />
+      <Image src={ANSILogo} alt="ANSI Logo" width={238} height={100} />
+      <Image src={SAELogo} alt="NAS Logo 3" width={106} height={67} />
     </div>
   );
 };
