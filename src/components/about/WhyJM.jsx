91835cdbea3410df685edb9e0ea97e2baaa6d912
SafeDuck <schopra579@gmail.com>
2024-08-09 18:07:06 -0700

    WhyJM responsive start

--- src/components/about/WhyJM.jsx
+++ src/components/about/WhyJM.jsx
@@ -8,68 +8,72 @@ import Image from "next/image";
 
 const WhyJM = () => {
   return (
-    <div className="w-2/3 flex flex-col justify-center gap-10 md:p-10">
+    <div className="w-5/6 md:w-2/3 flex flex-col justify-center gap-6 md:gap-10">
       <Header title="WHY J&M" size={"text-3xl"} />
-      <div className="flex justify-end ml-10">
-        <p>
-          At J&M response time is a critical part of total customer
-          satisfaction. We maintain a large inventory of finished parts ready
-          for immediate shipment. Delivery of large quantities can be made to
-          coincide with your critical production schedules.
-        </p>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="flex md:flex-row justify-center gap-4">
-          <Image src={JMbuilding} alt="Warehouse" className="w-1/2" />
-          <Image src={JMwarehouse} alt="Building" className="w-1/3" />
-        </div>
-        <div className="mt-2 text-center">
-          <p>pictures of J&M warehouse</p>
-        </div>
-      </div>
-      <div className="flex justify-end ml-10">
-        <p>
-          Requests for quote are handled in the same prompt manner. On our
-          standard product line, pricing and delivery information can usually be
-          given at the time of your request. Whether it is emailed, faxed, or
-          sent through our website all quotations receive the same prompt
-          attention.
-        </p>
-      </div>
-      <div className="flex flex-row items-start gap-4">
-        <div className="flex flex-col w-1/2 gap-2">
-          <div className="flex gap-2 justify-center">
-            <Image src={JMaward1} alt="Award1" className="w-1/4" />
-            <Image src={JMaward2} alt="Award2" className="w-1/4" />
+      <div className=" w-full flex flex-col justify-center items-end">
+        <div className="w-full md:w-11/12 flex flex-col items-center justify-center gap-2 md:gap-4">
+          <p>
+            At J&M response time is a critical part of total customer
+            satisfaction. We maintain a large inventory of finished parts ready
+            for immediate shipment. Delivery of large quantities can be made to
+            coincide with your critical production schedules.
+          </p>
+          <div className="flex flex-col items-center w-full">
+            <div className="flex md:flex-row justify-center gap-2 md:gap-4  w-full">
+              <Image src={JMbuilding} alt="Warehouse" className="w-1/2" />
+              <Image
+                src={JMwarehouse}
+                alt="Building"
+                className="w-1/2 md:w-1/3"
+              />
+            </div>
+            <p className="mt-2 text-center">pictures of J&M warehouse</p>
+          </div>
+          <p>
+            Requests for quote are handled in the same prompt manner. On our
+            standard product line, pricing and delivery information can usually
+            be given at the time of your request. Whether it is emailed, faxed,
+            or sent through our website all quotations receive the same prompt
+            attention.
+          </p>
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-row w-2/5 items-center justify-end">
+              <div className="flex flex-row gap-2 justify-center w-full">
+                <Image src={JMaward1} alt="Award1" className="w-2/5" />
+                <Image src={JMaward2} alt="Award2" className="w-2/5" />
+              </div>
+            </div>
+
+            <div className="flex flex-col w-3/5 gap-2">
+              <p className="text-left">
+                Customer Satisfaction is our goal. For over 60 years J&M has
+                provided a superior level of product performance with courteous
+                and efficient customer service. We hope to develop and nurture a
+                long term relationship with each of our customers. We are not
+                satisfied until our customers are satisfied. Every time.
+              </p>
+              <p className="text-left">
+                Customer Satisfaction is our goal. For over 60 years J&M has
+                provided a superior level of product performance with courteous
+                and efficient customer service. We hope to develop and nurture a
+                long term relationship with each of our customers. We are not
+                satisfied until our customers are satisfied. Every time.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-end ml-10">
+            <p>
+              We have a highly trained staff of QA personnel committed to
+              ensuring the highest possible level of quality in the products we
+              deliver. Our line support clamps are manufactured using the latest
+              techniques insuring they are made correctly every time. All
+              harness personnel are extensively trained using WHMA/IPC-A-620
+              acceptance criteria. The team approach is used to solve complex
+              manufacturing Issues and Lean Manufacturing principals are
+              employed throughout the production cycle.
+            </p>
           </div>
         </div>
-        <div className="flex flex-col w-1/2 gap-4">
-          <p className="text-left">
-            Customer Satisfaction is our goal. For over 60 years J&M has
-            provided a superior level of product performance with courteous and
-            efficient customer service. We hope to develop and nurture a long
-            term relationship with each of our customers. We are not satisfied
-            until our customers are satisfied. Every time.
-          </p>
-          <p className="text-left">
-            Customer Satisfaction is our goal. For over 60 years J&M has
-            provided a superior level of product performance with courteous and
-            efficient customer service. We hope to develop and nurture a long
-            term relationship with each of our customers. We are not satisfied
-            until our customers are satisfied. Every time.
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-end ml-10">
-        <p>
-          We have a highly trained staff of QA personnel committed to ensuring
-          the highest possible level of quality in the products we deliver. Our
-          line support clamps are manufactured using the latest techniques
-          insuring they are made correctly every time. All harness personnel are
-          extensively trained using WHMA/IPC-A-620 acceptance criteria. The team
-          approach is used to solve complex manufacturing Issues and Lean
-          Manufacturing principals are employed throughout the production cycle.
-        </p>
       </div>
     </div>
   );
