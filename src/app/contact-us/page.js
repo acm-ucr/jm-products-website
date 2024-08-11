77f91534043e36820d107b08d0e2ca8581d5851c
Jooahn Park <jpark8577@gmail.com>
2024-08-10 23:06:49 -0700

    Added button to ContactUs Page

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import FormButton from "@/components/FormButton";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <FormButton
+        text="SEND MESSAGE"
+        bgColor={"bg-jm-blue-200"}
+        borderColor={"border-jm-blue-200"}
+        textColor={"text-white"}
+        hoverBg={"group hover:!bg-white"}
+        hoverText={"group-hover:!text-jm-blue-200"}
+      />
+    </div>
+  );
 };
 
 export default page;
