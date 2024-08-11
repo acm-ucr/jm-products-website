69ec624a1e9142141db25b90340062b90eed0a1e
Jooahn Park <jpark8577@gmail.com>
2024-08-10 23:02:12 -0700

    Added FormButton Component

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const FormButton = () => {
-  return <div>FormButton</div>;
+const FormButton = ({
+  text,
+  bgColor,
+  borderColor,
+  textColor,
+  hoverBg,
+  hoverText,
+}) => {
+  return (
+    <button
+      className={`${bgColor} ${borderColor} border-2 rounded-md px-2.5 py-1 ${hoverBg}`}
+    >
+      <p
+        className={`${textColor} font-hk font-bold text-base tracking-widest text-center m-0 ${hoverText}`}
+      >
+        {text}
+      </p>
+    </button>
+  );
 };
 
 export default FormButton;
