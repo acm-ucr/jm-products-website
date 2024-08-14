bd9a3872b96cbc5458003deab6bbc4fee6d74c9e
Ezkron <eddie.leung246@gmail.com>
2024-08-14 00:52:37 -0700

    removed breaks, used tailwind classes for size, and removed size =

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -6,31 +6,26 @@ import JMbrochures from "/public/images/about/JMbrochures.webp";
 const WhoWeAre = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 py-8 flex flex-col gap-10">
-      <Header title="WHO WE ARE" size="text-4xl" />
+      <Header title="WHO WE ARE" />
 
       <div className="flex flex-col space-y-10">
         <p className="font-hk leading-relaxed text-base text-center md:text-left">
           J&M Products is dedicated to the turnkey manufacturing and marketing
-          of high quality products and services. <br />
-          Through leadership, innovation and continuous improvement we are able
-          to achieve the highest level <br />
-          of customer satisfaction. Our AS9100/ISO9001 approved quality
-          management system <br />
-          applies to all aspects of the company. Our goal is to deliver
-          defect-free product on-time at the most <br />
-          competitive price possible.
+          of high quality products and services. Through leadership, innovation
+          and continuous improvement we are able to achieve the highest level of
+          customer satisfaction. Our AS9100/ISO9001 approved quality management
+          system applies to all aspects of the company. Our goal is to deliver
+          defect-free product on-time at the most competitive price possible.
         </p>
 
         <p className="font-hk bg-gradient-to-r from-jm-blue-300 to-jm-blue-100 bg-clip-text text-transparent text-3xl md:text-4xl tracking-widest font-bold text-center">
           Brochures
         </p>
 
-        <div className="flex justify-center md:justify-end w-full">
+        <div className="w-full flex justify-center md:justify-end">
           <Image
             src={JMbrochures}
             alt="Brochures"
-            width={800}
-            height={600}
             className="w-full max-w-fit"
           />
         </div>
