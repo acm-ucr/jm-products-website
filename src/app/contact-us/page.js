e97f1f05090b073cd256eaca679cd74507562fca
Jooahn Park <jpark8577@gmail.com>
2024-08-13 22:32:14 -0700

    Added Info Component

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -4,7 +4,7 @@ import Info from "@/components/contact-us/Info";
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row justify-evenly items-center">
+    <div className="w-full h-screen flex flex-col md:flex-row justify-evenly items-center">
       <Form />
       <Info />
     </div>
