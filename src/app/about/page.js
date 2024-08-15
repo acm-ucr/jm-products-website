5884571d8fbda21feb07d6309eedf0e8ee0cac68
Sujay Subramanyam <ssubr022@gmail.com>
2024-08-15 16:23:50 -0700

    Need to get rid of pixels and change color

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,10 +2,12 @@ import WhoWeAre from "@/components/about/WhoWeAre";
 import React from "react";
 import WhyJM from "@/components/about/WhyJM";
 import Information from "@/components/about/Information";
+import HexLogo from "@/components/about/HexLogo";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-10">
+      <HexLogo />
       <WhoWeAre />
       <WhyJM />
       <Information />
--- src/components/about/HexLogo.jsx
+++ src/components/about/HexLogo.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import BackgroundImage from "@/public/images/about/HexBackground.webp";
+import ProductLogo from "@/public/svg/J&MLogo.svg";
 
 const HexLogo = () => {
-  return <div>HexLogo</div>;
+  return (
+    <div className="relative w-[390px] h-[390px]">
+      <div className="absolute inset-0 z-10 flex items-center justify-center flex-col space-y-4 font-family: Georgia ">
+        <h1 className="text-white">ABOUT J&M</h1>
+        <Image src={ProductLogo} objectFit="cover" alt="Background Image" />
+      </div>
+      <Image
+        className="absolute inset-0 z-0"
+        src={BackgroundImage}
+        layout="fill"
+        objectFit="cover"
+        alt="Background Image"
+      />
+    </div>
+  );
 };
 
 export default HexLogo;
