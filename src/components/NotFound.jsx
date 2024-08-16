65736d0fa209d1b5bcd16b7ec62cb048b4975311
javi <javi@Javis-MBP-3.attlocal.net>
2024-08-15 22:09:05 -0700

    Fix Button appearance and positioning on all screen sizes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,27 +7,25 @@ import Decor3 from "@/public/svg/404Decor.svg";
 
 const NotFound = () => {
   return (
-    <div className="relative w-full min-h-24 sm:min-h-28 md:min-h-screen justify-center overflow-hidden content-center ">
-      <div className="relative flex flex-col items-center justify-center font-hk text-white tracking-widest text-center">
-        <Image src={Hex404} alt="404 Hex" className="w-3/4 md:w-2/3 p-2 z-0" />
-        <p className="absolute text-6xl md:text-7xl lg:text-8xl xl:text-9xl top-16 md:top-20 lg:top-32 font-semibold z-10">
+    <div className="flex relative w-full min-h-24 sm:min-h-28 md:min-h-screen justify-center items-center overflow-hidden">
+      <Image src={Hex404} alt="404 Hex" className="w-3/4 md:w-2/3 p-2 z-0" />
+      <div className="absolute flex flex-col items-center justify-center font-hk text-white tracking-widest text-center z-10">
+        <p className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-semibold mb-4">
           404
         </p>
-        <p className="absolute w-1/2 sm:w-1/3 text-xs sm:text-lg lg:text-2xl xl:text-3xl top-36 md:top-44 lg:top-64 xl:top-72 font-light z-10">
+        <p className="w-1/2 sm:w-1/3 text-xs sm:text-lg lg:text-2xl xl:text-3xl font-light mb-6">
           The page you are looking for does not exist!
         </p>
-        <div className="absolute w-full bottom-14">
-          <Button
-            text="Back to home"
-            link="/"
-            bgColor={"bg-jm-blue-300"}
-            borderColor={"border-black"}
-            textColor={"text-black"}
-            hoverBg={"group hover:!bg-jm-blue-100"}
-            hoverText={"group-hover:!text-white"}
-            className="font-hk p-2 z-20"
-          />
-        </div>
+        <Button
+          text="Back to home"
+          link="/"
+          bgColor={"bg-jm-blue-300"}
+          borderColor={"border-black"}
+          textColor={"text-black"}
+          hoverBg={"group hover:!bg-jm-blue-100"}
+          hoverText={"group-hover:!text-white"}
+          className="font-hk p-2"
+        />
       </div>
 
       <div className="absolute overflow-hidden left-0 -top-32 md:-top-8 lg:-top-6 xl:-top-4 hidden sm:block">
