6c5923819161ce2e18b94cd384fbfe232686f953
Jooahn Park <jpark8577@gmail.com>
2024-08-17 15:57:45 -0700

    Added gradient color to form button

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -5,9 +5,9 @@ const FormButton = () => {
     <input
       type="submit"
       value="SEND MESSAGE"
-      className={`bg-jm-blue-200 border-jm-blue-200 border-2 
-        rounded-md px-2.5 py-1 group hover:!bg-white text-white font-hk 
-        font-bold text-base tracking-widest text-center m-0 hover:!text-jm-blue-200`}
+      className={`bg-gradient-to-r from-jm-blue-200 to-jm-blue-300 border-2
+        rounded-md px-2.5 py-1 group hover:!bg-none text-white font-hk 
+        font-bold text-base tracking-widest text-center m-0 hover:!text-jm-blue-200 hover:!border-jm-blue-200`}
     />
   );
 };
