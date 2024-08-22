88bc8697179d5c06123b24c3f8bd42ee6d23feb0
Avia Bequette <aviabequette@Avias-MacBook-Pro.local>
2024-08-23 03:30:33 +0900

    animated descriptions, bulletpoints, pdflink, and logostrip components

--- src/components/PDFLink.jsx
+++ src/components/PDFLink.jsx
@@ -1,17 +1,34 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { IoDocumentTextOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
+
+const pdfAnimation = {
+  start: {
+    opacity: 0,
+    y: -30,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const PDFLink = ({ text, link, textColor, hoverColor }) => {
   return (
-    <div
+    <motion.div
       className={`${textColor} underline flex flex-row gap-2 lg:gap-3 ${hoverColor} items-center`}
+      variants={pdfAnimation}
+      initial="start"
+      whileInView="end"
+      transition={{ duration: 0.2, delay: 0.3 }}
     >
       <Link href={link} target="_blank">
         {text}
       </Link>
       <IoDocumentTextOutline className="flex-shrink-0" />
-    </div>
+    </motion.div>
   );
 };
 
