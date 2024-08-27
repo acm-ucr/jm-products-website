2149c8120d5f5dedafbb8c48803f152e68300300
Eddie Leung <121210286+Ezkron@users.noreply.github.com>
2024-08-27 00:36:59 -0700

    Update WhoWeAre.jsx

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -26,7 +26,7 @@ const fadeInFromTop = {
 
 const WhoWeAre = () => {
   return (
-    <div className="w-5/6 md:w-2/3 flex flex-col gap-10">
+    <div className="max-w-7xl mx-auto px-4 py-8 flex flex-col gap-10">
       <motion.div
         variants={SlideInFromLeft}
         viewport={{ once: true, amount: 0 }}
@@ -38,6 +38,26 @@ const WhoWeAre = () => {
       </motion.div>
 
       <div className="flex flex-col space-y-10">
+        <p className="font-hk leading-relaxed text-left md:text-center text-xs md:text-base">
+          J&M Products is dedicated to the turnkey manufacturing and marketing
+          of high quality products and services. Through leadership, innovation
+          and continuous improvement we are able to achieve the highest level of
+          customer satisfaction. Our AS9100/ISO9001 approved quality management
+          system applies to all aspects of the company. Our goal is to deliver
+          defect-free product on-time at the most competitive price possible.
+        </p>
+
+        <p className="font-hk bg-gradient-to-r from-jm-blue-300 to-jm-blue-100 bg-clip-text text-transparent text-sm md:text-3xl tracking-widest font-bold text-left md:text-center">
+          Brochures
+        </p>
+
+        <div className="w-full flex justify-center md:justify-end">
+          <Image
+            src={JMbrochures}
+            alt="Brochures"
+            className="w-full max-w-fit"
+          />
+        </div>
       </div>
     </div>
   );
