8fa9158940bb4df7dfb975712820b81bfb00371a
Ezkron <eddie.leung246@gmail.com>
2024-08-29 21:25:42 -0700

    hopefully this does not merge conflict

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -7,7 +7,7 @@ import JMbrochures from "/public/images/about/JMbrochures.webp";
 import { motion } from "framer-motion";
 
 const SlideInFromLeft = {
-  initial: { opacity: 0, x: -1000 },
+  initial: { opacity: 0, x: -50 },
   animate: (custom) => ({
     opacity: 1,
     x: 0,
@@ -38,13 +38,51 @@ const WhoWeAre = () => {
       </motion.div>
 
       <div className="flex flex-col space-y-10">
-        <div className="w-full flex justify-center md:justify-end">
-          <Image
-            src={JMbrochures}
-            alt="Brochures"
-            className="w-full max-w-fit"
-          />
-        </div>
+        <motion.div
+          variants={fadeInFromTop}
+          viewport={{ once: true, amount: 0 }}
+          initial="initial"
+          whileInView="animate"
+          custom={0.3}
+        >
+          <p className="font-hk text-left md:text-center text-xs md:text-base font-medium ">
+            J&M Products is dedicated to the turnkey manufacturing and marketing
+            of high quality products and services. Through leadership,
+            innovation and continuous improvement we are able to achieve the
+            highest level of customer satisfaction. Our AS9100/ISO9001 approved
+            quality management system applies to all aspects of the company. Our
+            goal is to deliver defect-free product on-time at the most
+            competitive price possible.
+          </p>
+        </motion.div>
+
+        <motion.div
+          variants={fadeInFromTop}
+          viewport={{ once: true, amount: 0 }}
+          initial="initial"
+          whileInView="animate"
+          custom={0.4}
+        >
+          <p className="font-hk bg-gradient-to-r from-jm-blue-300 to-jm-blue-100 bg-clip-text text-transparent text-sm md:text-3xl tracking-widest font-bold text-left md:text-center">
+            Brochures
+          </p>
+        </motion.div>
+
+        <motion.div
+          variants={fadeInFromTop}
+          viewport={{ once: true, amount: 0 }}
+          initial="initial"
+          whileInView="animate"
+          custom={0.5}
+        >
+          <div className="w-full flex justify-center md:justify-end">
+            <Image
+              src={JMbrochures}
+              alt="Brochures"
+              className="w-full max-w-fit"
+            />
+          </div>
+        </motion.div>
       </div>
     </div>
   );
